71c0c18e6ce5a4bf0494a2ce4ce0146935331bcb
Aaron Chan <aarchan108@gmail.com>
2024-09-12 15:55:48 -0400

    Fix interface naming

--- lib/db/db.go
+++ lib/db/db.go
@@ -2,7 +2,7 @@ package db
 
 import "github.com/AarC10/GSW-V2/proc"
 
-type DbHandler interface {
+type Handler interface {
 	Insert(packet []proc.TelemetryPacket)
 	CreateQuery(packet proc.TelemetryPacket) string
 }
