51619e2c893a9385c4ec84cca6d8aa9f288f3bcf
Aaron Chan <aarchan108@gmail.com>
2024-09-16 19:36:12 -0400

    Fixes to service installer script

--- create_service.sh
+++ create_service.sh
@@ -6,7 +6,7 @@ touch $servicefile
 pwd
 { printf "[Unit]\n"; printf "Description=RIT Launch Ground Software Service\n\n"; } >> $servicefile
 
-{ printf "[Serivce]\n"; printf "Type=Simple\n"; } >> $servicefile
+{ printf "[Service]\n"; printf "Type=Simple\n"; } >> $servicefile
 if chmod 777 gsw_service;
 then
     echo "gsw_service exists"
@@ -14,7 +14,8 @@ else
     echo "gsw_service not found, exiting"
     exit 1
 fi
-{ printf "ExecStart=%s/gsw_service\n" "$(pwd)"; printf "Restart=on-failure\n\n"; } >> $servicefile
+{ printf "Restart=on-failure\n"; }
+{ printf "ExecStart=%s/gsw_service\n" "$(pwd)"; printf "WorkingDirectory=%s\n\n" "$(pwd)"; } >> $servicefile
 
 { printf "[Install]\n"; printf "WantedBy=multi-user.target\n"; } >> $servicefile
 
