42ed126ee4fcceeb442fd47ab51bd02306c88539
Aiden Penza <amp6792@rit.edu>
2024-11-05 21:20:34 -0500

    fixed formatting

--- cmd/mem_view/mem_view.go
+++ cmd/mem_view/mem_view.go
@@ -52,7 +52,7 @@ func printTelemetryPacket(startLine int, packet tlm.TelemetryPacket, rcvChan cha
 func main() {
 	configReader, err := ipc.CreateIpcShmReader("config")
 	if err != nil {
-			fmt.Println("*** Error accessing config file. Make sure the GSW service is running. ***")
+		fmt.Println("*** Error accessing config file. Make sure the GSW service is running. ***")
 		fmt.Printf("(%v)\n", err)
 		return
 	}
