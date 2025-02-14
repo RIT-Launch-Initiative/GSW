ebafc4f3ae1d4f599b40bf939ecab510993577c7
Aiden Penza <amp6792@rit.edu>
2025-02-13 23:14:51 -0500

    added payload loop and cleaned up code + formatting

--- cmd/udp_command/udp_command.go
+++ cmd/udp_command/udp_command.go
@@ -5,16 +5,11 @@ import (
 	"fmt"
     "net"
 	"strconv"
-	"encoding/binary"
 	"strings"
 	"bufio"
 )
 
-const (
-    radio_port = 12000
-)
-
-func OpenConn(host string, port int) (*net.UDPConn, error) {
+func openConn(host string, port int) (*net.UDPConn, error) {
     addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%v:%v", host, port))
     if err != nil {
         return nil, fmt.Errorf("Error resolving UDP address: %v", err)
@@ -26,76 +21,77 @@ func OpenConn(host string, port int) (*net.UDPConn, error) {
     return conn, nil;
 }
 
-func SendOverUDP(conn *net.UDPConn, packet []byte) error {
+func sendOverUDP(conn *net.UDPConn, packet []byte) error {
     _, err := conn.Write(packet)
     if err != nil {
-        return fmt.Errorf("Error sending data to radio: %v", err)
+        return err
     }
     return nil
 }
 
-func CloseConn(conn *net.UDPConn) {
+func closeConn(conn *net.UDPConn) {
    err := conn.Close()
    if err != nil {
-       fmt.Printf("Error closing connection: %v", err)
+       fmt.Println("Error closing connection:", err)
    }
 }
 
 func main() {
 	var host string
 	var port string
-
-	fmt.Print("IP Address: ")
-	_, err := fmt.Scanln(&host)
-	if err != nil {
-        fmt.Println(err)
-		return
-	}
-	fmt.Print("Port: ")
-	_, err = fmt.Scanln(&port)
-	if err != nil {
-        fmt.Println(err)
-		return
-	}
-
-	fmt.Print("Payload: ")
 	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("IP address: ")
 	scanner.Scan()
-	err = scanner.Err()
-	if err != nil {
-		fmt.Println(err)
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading IP address:", err)
 		return
 	}
-	payloadStr := scanner.Text()
-
-	payload := make([]byte, 0, 10)
-	byteToAdd :=  make([]byte, 8)
-	for _, str := range strings.Fields(payloadStr) {
-		parsedInt, err := strconv.ParseUint(str, 0, 64)
-		if err != nil {
-        	fmt.Println(err)
-			return
-		}
-		binary.LittleEndian.PutUint64(byteToAdd, parsedInt)
-		payload = append(payload, byteToAdd[0])
+	host = scanner.Text()
+	fmt.Print("Port: ")
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading port:", err)
+		return
 	}
+	port = scanner.Text()
 
 	parsedPort, err := strconv.ParseInt(port, 0, 0)
 	if err != nil {
-        fmt.Println(err)
+		fmt.Println("Error parsing port:", err)
 		return
 	}
-    conn, err := OpenConn(host, int(parsedPort))
+    conn, err := openConn(host, int(parsedPort))
     if err != nil {
-        fmt.Println(err)
+		fmt.Println("Error opening connection:", err)
 		return
     }
-    defer CloseConn(conn)
+    defer closeConn(conn)
+	fmt.Println("Connection opened.\n")
 
-	fmt.Printf("Sending payload %v\n", payload)
-    err = SendOverUDP(conn, payload)
-    if err != nil {
-        fmt.Println(err)
-		return
-    }
+	PayloadLoop:
+	for {
+		fmt.Print("Payload: ")
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			fmt.Println("\tError reading payload:", err)
+			continue
+		}
+		payloadStr := scanner.Text()
+
+		payload := make([]byte, 0, 10)
+		for _, str := range strings.Fields(payloadStr) {
+			parsedInt64, err := strconv.ParseUint(str, 0, 8)
+			if err != nil {
+				fmt.Println("\tError parsing payload:", err)
+				continue PayloadLoop
+			}
+			payload = append(payload, uint8(parsedInt64))
+		}
+		fmt.Printf("\tSending payload %# x\n", payload)
+		if err := sendOverUDP(conn, payload); err != nil {
+			fmt.Println("\tError sending payload:", err)
+			continue
+		}
+	}
 }
