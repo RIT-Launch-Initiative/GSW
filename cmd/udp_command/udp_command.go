85e729ccd6ec1a0a84ff1542bd171ba2891ca703
Aiden Penza <amp6792@rit.edu>
2025-02-13 23:24:30 -0500

    added comments and removed closeConn (it was unused and unnecessary)

--- cmd/udp_command/udp_command.go
+++ cmd/udp_command/udp_command.go
@@ -9,6 +9,7 @@ import (
 	"bufio"
 )
 
+// "Opens a connection" over UDP with the specified host and port
 func openConn(host string, port int) (*net.UDPConn, error) {
     addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%v:%v", host, port))
     if err != nil {
@@ -21,6 +22,7 @@ func openConn(host string, port int) (*net.UDPConn, error) {
     return conn, nil;
 }
 
+// Sends the given packet over the given connection
 func sendOverUDP(conn *net.UDPConn, packet []byte) error {
     _, err := conn.Write(packet)
     if err != nil {
@@ -29,18 +31,12 @@ func sendOverUDP(conn *net.UDPConn, packet []byte) error {
     return nil
 }
 
-func closeConn(conn *net.UDPConn) {
-   err := conn.Close()
-   if err != nil {
-       fmt.Println("Error closing connection:", err)
-   }
-}
-
 func main() {
 	var host string
 	var port string
 	scanner := bufio.NewScanner(os.Stdin)
 
+	// Read in host and port
 	fmt.Print("IP address: ")
 	scanner.Scan()
 	if err := scanner.Err(); err != nil {
@@ -56,6 +52,7 @@ func main() {
 	}
 	port = scanner.Text()
 
+	// Parse port to int + open connection
 	parsedPort, err := strconv.ParseInt(port, 0, 0)
 	if err != nil {
 		fmt.Println("Error parsing port:", err)
@@ -66,9 +63,9 @@ func main() {
 		fmt.Println("Error opening connection:", err)
 		return
     }
-    defer closeConn(conn)
 	fmt.Println("Connection opened.\n")
 
+	// Loop prompting for payload
 	PayloadLoop:
 	for {
 		fmt.Print("Payload: ")
@@ -79,6 +76,7 @@ func main() {
 		}
 		payloadStr := scanner.Text()
 
+		// Loop adding byte from input line to payload
 		payload := make([]byte, 0, 10)
 		for _, str := range strings.Fields(payloadStr) {
 			parsedInt64, err := strconv.ParseUint(str, 0, 8)
@@ -88,6 +86,8 @@ func main() {
 			}
 			payload = append(payload, uint8(parsedInt64))
 		}
+
+		// Send payload over connection
 		fmt.Printf("\tSending payload %# x\n", payload)
 		if err := sendOverUDP(conn, payload); err != nil {
 			fmt.Println("\tError sending payload:", err)
