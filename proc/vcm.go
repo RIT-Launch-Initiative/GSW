701f88fed52f64b068457d5147c53cac0725bb9d
Aiden Penza <amp6792@rit.edu>
2025-04-15 20:50:10 -0400

    handled yaml unmarshaling error

--- proc/vcm.go
+++ proc/vcm.go
@@ -34,7 +34,10 @@ func ParseConfig(filename string) (*Configuration, error) {
 // ParseConfigBytes parses a YAML formatted byte slice and returns a Configuration struct
 func ParseConfigBytes(data []byte) (*Configuration, error) {
 	// Unmarshalling doesn't seem to lead to errors with bad data. Better to check result config
-	_ = yaml.Unmarshal(data, &GswConfig)
+	err := yaml.Unmarshal(data, &GswConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling YAML: %v", err)
+	}
 	if GswConfig.Name == "" {
 		return nil, fmt.Errorf("Error parsing YAML. No config name.")
 	}
