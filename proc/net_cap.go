c6b79561f70cb4c772f9d806fe3a7731f63d907a
Aaron Chan <aarchan108@gmail.com>
2026-02-23 11:53:18 -0500

    set up pcap handle

--- proc/net_cap.go
+++ proc/net_cap.go
@@ -1,14 +1,23 @@
 package proc
 
-import "strconv"
+import (
+	"fmt"
+	"github.com/google/gopacket/pcap"
+	"strings"
+)
 
 func NetworkCapture() {
 	ports := []string{}
 	for _, packet := range GswConfig.TelemetryPackets {
-		ports = append(ports, strconv.Itoa(packet.Port))
+		ports = append(ports, fmt.Sprintf("udp port %d", packet.Port))
 	}
 
-	for {
+	filter := strings.Join(ports, " or ")
 
+	handle, err := pcap.OpenLive("any", 1600, true, pcap.BlockForever)
+	if err != nil {
+		fmt.Printf("Error opening pcap handle: %v\n", err)
+		return
 	}
+
 }
